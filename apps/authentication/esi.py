3731d707643f08ab4f0f45f1d087300f487f56bd
Brian Merriam <merriam@gmail.com>
2022-05-17 03:58:01 +0000

    added wallet summary on dashboard

--- apps/authentication/esi.py
+++ apps/authentication/esi.py
@@ -7,16 +7,17 @@ class EsiAuth:
     def __init__(self):
         self.esisecurity = None
         self.esiclient = None
+        self.esiapp = None
         
     
     def init_app(self, app):
         
         
-        esiapp = esipy.App.create(app.config['ESI_SWAGGER_JSON'])
+        self.esiapp = esipy.App.create(app.config['ESI_SWAGGER_JSON'])
 
         # init the security object
         self.esisecurity = esipy.EsiSecurity(
-            app=esiapp,
+            app=self.esiapp,
             redirect_uri=app.config['ESI_CALLBACK'],
             client_id=app.config['ESI_CLIENT_ID'],
             secret_key=app.config['ESI_SECRET_KEY'],
@@ -27,4 +28,19 @@ class EsiAuth:
         self.esiclient = esipy.EsiClient(
             security=self.esisecurity, cache=None, headers={"User-Agent": app.config['ESI_USER_AGENT']}
         )
-        
+
+    def get_wallet(self, current_user):
+        """Get a users wallet transactions
+
+        Args:
+            current_user (UsersModel): the current user from the Users login_manager Model
+
+        Returns:
+            string: json of wallet transactions
+        """        
+        self.esisecurity.update_token(current_user.get_sso_data())
+
+        op = self.esiapp.op['get_characters_character_id_wallet'](
+            character_id=current_user.character_id
+        )
+        return self.esiclient.request(op)
