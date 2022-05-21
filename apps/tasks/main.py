77e1d64d9449fb0b95725732e9e8129099a4d54c
Brian Merriam <merriam@gmail.com>
2022-05-21 04:33:09 +0000

    added main tasks class

---
+++ apps/tasks/main.py
@@ -0,0 +1,36 @@
+"""This is the main driving class of the overall tasks.
+
+Here we will have a root function which is called on a schedule, that will then call subtasks which 
+will qualify if they run or not based on their own schedule. Meaning, ome updates will happen more
+frequently than others.
+"""
+
+
+class MainTasks:
+    """The Main tasks driving class.  
+    
+    We intialize, control and execute our tasks here.
+    """
+    def __init__(self):
+        """Run internal class intialization functions
+        """        
+        self.load_tasks()
+    
+
+    def run_tasks(self):
+        """Execute all of our tasks.
+        
+        This will run all the tasks. They could be API updates,
+        notifications or clean up jobs.
+        """        
+        pass
+    
+    def load_tasks(self):
+        """Run any potential initilization tasks
+        """        
+        pass
+    
+    def purge_tasks(self):
+        """Purge all tasks in the queue
+        """        
+        pass
