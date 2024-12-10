9a00e27a4bfcbb6dcc6a001d5fec61e905fd4b5a
TargetedEntropy <merriam@gmail.com>
2024-12-10 00:51:40 +0000

    fix time for skills task

--- apps/tasks/modules/skills.py
+++ apps/tasks/modules/skills.py
@@ -15,7 +15,7 @@ class SkillTasks:
         self.scheduler.add_job(
             func=self.main,
             trigger="interval",
-            seconds=10,
+            seconds=3600,
             id="skill_main",
             name="skill_main",
             replace_existing=False,
