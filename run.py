843eed91d178d90ce13cd531c5278d94ab1cd77d
TargetedEntropy <merriam@gmail.com>
2024-12-10 02:55:31 +0000

    cleanup

--- run.py
+++ run.py
@@ -3,8 +3,8 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+import sys
 from flask_migrate import Migrate
-from sys import exit
 from decouple import config
 
 from apps.config import config_dict
@@ -15,14 +15,14 @@ from apps import create_app, db
 DEBUG = config("DEBUG", default=True, cast=bool)
 
 # The configuration
-get_config_mode = "Debug" if DEBUG else "Production"
+GET_CONFIG_MODE = "Debug" if DEBUG else "Production"
 
 try:
     # Load the configuration using the default values
-    app_config = config_dict[get_config_mode.capitalize()]
+    app_config = config_dict[GET_CONFIG_MODE.capitalize()]
 
 except KeyError:
-    exit("Error: Invalid <config_mode>. Expected values [Debug, Production] ")
+    sys.exit("Error: Invalid <config_mode>. Expected values [Debug, Production] ")
 
 app_config.PREFERRED_URL_SCHEME = "https"
 
@@ -32,7 +32,7 @@ Migrate(app, db)
 
 if DEBUG:
     app.logger.info("DEBUG       = " + str(DEBUG))
-    app.logger.info("Environment = " + get_config_mode)
+    app.logger.info("Environment = " + GET_CONFIG_MODE)
     app.logger.info("DBMS        = " + app_config.SQLALCHEMY_DATABASE_URI)
     app.logger.info("PREFERRED_URL_SCHEME = " + app_config.PREFERRED_URL_SCHEME)
 
